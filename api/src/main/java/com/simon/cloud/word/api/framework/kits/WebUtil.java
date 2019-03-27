f621f9deb9526165369b6f9709d85bd1a5f1ba2f
weihaibo <554040883@qq.com>
2019-03-27 16:31:56 +0800

    update mapper

--- api/src/main/java/com/simon/cloud/word/api/framework/kits/WebUtil.java
+++ api/src/main/java/com/simon/cloud/word/api/framework/kits/WebUtil.java
@@ -16,7 +16,7 @@ public class WebUtil {
     private static final String X_REQUESTED_WITH = "X-Requested-With";
 
     private static final String CONTENT_TYPE = "Content-type";
-    private static final String CONTENT_TYPE_JSON = "application/json";
+    private static final String CONTENT_TYPE_JSON = "application/json;charset=UTF-8";
 
     public static boolean isAjax(HttpServletRequest request) {
         return XML_HTTP_REQUEST.equals(request.getHeader(X_REQUESTED_WITH));
